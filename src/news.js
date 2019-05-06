10fe3c97bf144294d96860a9cdc20e1391dfcbd7
Vince Charals <vincecharals@gmail.com>
2019-05-06 18:37:28 -0500

    added initial files with homepage mostly done

---
+++ src/news.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./news.css";
+
+function News() {
+  return (
+    <div>
+      <h3>Today's News</h3>
+    </div>
+  );
+}
+export default News;
